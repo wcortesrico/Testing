74cc32991af438ca7ac24c7840aee02af79d7805
Ell-716 <elenabai.2021@gmail.com>
2025-03-25 14:22:18 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,15 @@
+# Requirements
+# You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
+# The rover receives a character array of commands.
+#
+# Implement commands that move the rover forward/backward (f,b).
+# Implement commands that turn the rover left/right (l,r).
+# Implement wrapping at edges. But be careful, planets are spheres.
+# Implement obstacle detection before each move to a new square.
+# If a given sequence of commands encounters an obstacle,
+# the rover moves up to the last possible point, aborts the sequence and reports the obstacle.
+
+
 from palindrom import palindrom
 from rover import Rover
 
@@ -9,4 +21,5 @@ def test_palindrom():
 
 def test_rover():
     assert Rover().position(0, 0) == [0, 0]
+
 
